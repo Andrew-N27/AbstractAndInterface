f92396a4c24ed119fb839f52c0928e4830ce921e
Andrii Nasheba <a.nasheba@xtoniq.com>
2023-04-13 19:10:47 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	interface IDrive
+	{
+		int MaxSpeed { get; }
+
+		void Drive();
+	}
+	public abstract class Car : IDrive
+	{
+		public virtual string Name { get; }
+
+		public virtual string EngineType { get; }
+
+		public virtual double Price { get; }
+
+		public virtual string Body { get; }
+
+		public virtual string Description { get; }
+
+		public virtual int MaxSpeed { get; } 
+
+		public virtual void Drive()
+		{
+			Console.WriteLine("Drive...");
+		}
+	}
+
+	public class BmwCar : Car
+	{
+
+		public override string Name { get { return "BMW M5"; } }
+
+		public override string EngineType { get { return "Gas"; } }
+
+		public override double Price { get { return 9999.9; } }
+
+		public override string Body { get { return "Sedan"; } }
+
+		public override string Description { get { return "Auto"; } }
+
+		public override int MaxSpeed { get { return 220; } }
+
+		public override void Drive()
+		{
+			Console.WriteLine($"Drive: {Name}");
+		}
+
+	}
+
+	public class MersCar : Car
+	{
+
+		public override string Name { get { return "Mers 140"; } }
+
+		public override string EngineType { get { return "Gas"; } }
+
+		public override double Price { get { return 7599.9; } }
+
+		public override string Body { get { return "Sedan"; } }
+
+		public override string Description { get { return "Auto"; } }
+
+		public override int MaxSpeed { get { return 230; } }
+
+		public override void Drive()
+		{
+			Console.WriteLine($"Drive: {Name}");
+		}
+
+	}
+
+	public class AudiCar : Car
+	{
+
+		public override string Name { get { return "Audi A5"; } }
+
+		public override string EngineType { get { return "Gas"; } }
+
+		public override double Price { get { return 8572.9; } }
+
+		public override string Body { get { return "Sedan"; } }
+
+		public override string Description { get { return "Auto"; } }
+
+		public override int MaxSpeed { get { return 225; } }
+
+		public override void Drive()
+		{
+			Console.WriteLine($"Drive: {Name}");
+		}
+
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			Console.WriteLine("1 - BMW");
+			Console.WriteLine("2 - Mers");
+			Console.WriteLine("3 - Audi");
+
+			int num = int.Parse(Console.ReadLine());
+
+			switch (num)
+			{
+				case 1: //BMW
+					Show(new BmwCar());
+					break;
+				case 2: //Mers
+					Show(new MersCar());
+					break;
+				case 3: //Audi
+					Show(new AudiCar());
+					break;
+				default: Console.WriteLine("Error...");
+					break;
+			}
+		}
+
+		private static void Show(Car car)
+		{
+			Console.WriteLine($"Name: {car.Name}");
+			Console.WriteLine($"EngineType: {car.EngineType}");
+			Console.WriteLine($"Price: {car.Price}");
+			Console.WriteLine($"Body: {car.Body}");
+			Console.WriteLine($"Description: {car.Description}");
+			Console.WriteLine($"MaxSpeed: {car.MaxSpeed}");
+			car.Drive();
+		} 
+	}
+}
